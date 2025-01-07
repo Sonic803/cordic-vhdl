b789452cb5943cfb128a95dc22999fe5e8220b7c
Sonic803 <sonic82003@gmail.com>
2025-01-07 10:19:55 +0100

    Full lut table

--- vhdl/python/atan_generation.py
+++ vhdl/python/atan_generation.py
@@ -9,7 +9,7 @@ use IEEE.numeric_std.all;
 
 ENTITY {mname} is
   port (
-    address  : in  std_logic_vector({length} downto 0);
+    address  : in  std_logic_vector({addr_bits} downto 0);
     lut_out : out std_logic_vector({width} downto 0)
   );
 end entity;
@@ -31,22 +31,28 @@ END PROCESS;
 end architecture;
 """
 
-# TODO make it so the whole table is filled
+# if you dont want to fill the whole table
+# remember to change the architecture to behavioral
+# IF to_integer(unsigned(address)) <= {lines} THEN
+#   lut_out <= STD_LOGIC_VECTOR(to_signed(LUT(to_integer(unsigned(address))), {actual_width}));
+# ELSE
+#   lut_out <= (others => '0');
+# END IF;
 
 line = "{addr} => {val},"
 
 M = 24
 N = 16
 FRAC = 8
-angle_FRAC=M-3
+angle_FRAC = M - 3
 ITERATIONS = 24
 
 mname = "ATAN_LUT"
-length = int(math.log2(ITERATIONS - 1))
+addr_bits = int(math.log2(ITERATIONS - 1)) + 1
 width = M
-lines = ITERATIONS - 1
+lines = 2**addr_bits
 
-values = [int(math.atan(2 ** (-i)) * 2**(angle_FRAC)) for i in range(ITERATIONS)]
+values = [int(math.atan(2 ** (-i)) * 2 ** (angle_FRAC)) for i in range(2**addr_bits)]
 
 # check that the values are in the range of the width
 for i in range(len(values)):
@@ -61,9 +67,9 @@ lut = "\n\t".join([line.format(addr=i, val=value) for i, value in enumerate(valu
 
 result = stringa.format(
     mname=mname,
-    length=length,
+    addr_bits=addr_bits - 1,
     width=width - 1,
-    lines=lines,
+    lines=lines - 1,
     lut=lut,
     actual_width=width,
 )
@@ -72,16 +78,16 @@ with open(f"vhdl/src/{mname}.vhd", "w") as f:
     f.write(result)
 
 
-An = 1
+# An = 1
 
-for i in range(0, ITERATIONS):
-    An = An * math.sqrt(1 + 1 / (2**i) ** 2)
+# for i in range(0, ITERATIONS):
+#     An = An * math.sqrt(1 + 1 / (2**i) ** 2)
 
-print(f"An = {An}")
-k = 1 / An
-# k = (int(k * 2 ** (N - 1))) / (2**(N-1))
-# print('{:.32f}'.format(k))
-print(f"k = {k}")
+# print(f"An = {An}")
+# k = 1 / An
+# # k = (int(k * 2 ** (N - 1))) / (2**(N-1))
+# # print('{:.32f}'.format(k))
+# print(f"k = {k}")
 
-k=int(k*2**31)
-print(f"k = {k}")
+# k=int(k*2**31)
+# print(f"k = {k}")
--- vhdl/src/ATAN_LUT.vhd
+++ vhdl/src/ATAN_LUT.vhd
@@ -12,7 +12,7 @@ end entity;
 
 architecture rtl of ATAN_LUT is
 
-  type LUT_t is array (natural range 0 to 23) of integer;
+  type LUT_t is array (natural range 0 to 31) of integer;
   constant LUT: LUT_t := (
 	0 => 1647099,
 	1 => 972339,
@@ -37,7 +37,15 @@ architecture rtl of ATAN_LUT is
 	20 => 1,
 	21 => 0,
 	22 => 0,
-	23 => 0
+	23 => 0,
+	24 => 0,
+	25 => 0,
+	26 => 0,
+	27 => 0,
+	28 => 0,
+	29 => 0,
+	30 => 0,
+	31 => 0
 	  );
 
 begin
