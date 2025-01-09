72e76e5472604b0c1433e46f64c84bb69c604ccc
Sonic803 <sonic82003@gmail.com>
2025-01-09 10:56:18 +0100

    Tabs

--- vhdl/src/CORDIC.vhd
+++ vhdl/src/CORDIC.vhd
@@ -3,209 +3,208 @@ USE ieee.std_logic_1164.ALL;
 USE ieee.numeric_std.ALL;
 ENTITY CORDIC IS
 
-    GENERIC (
-        M : POSITIVE := 24; -- internal representation
-        N : POSITIVE := 16; -- input size
-        ITERATIONS : POSITIVE := 16; -- CORDIC algorithm iterations
-        ITER_BITS : POSITIVE := 4 -- number of bits needed to represent iterations
-    );
-    PORT (
-        clk : IN STD_LOGIC;
-        rst : IN STD_LOGIC;
-        x : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-        y : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-        start : IN STD_LOGIC;
-        rho : OUT STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-        theta : OUT STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-        valid : OUT STD_LOGIC
-    );
+  GENERIC (
+    M : POSITIVE := 24; -- internal representation
+    N : POSITIVE := 16; -- input size
+    ITERATIONS : POSITIVE := 16; -- CORDIC algorithm iterations
+    ITER_BITS : POSITIVE := 4 -- number of bits needed to represent iterations
+  );
+  PORT (
+    clk : IN STD_LOGIC;
+    rst : IN STD_LOGIC;
+    x : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+    y : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+    start : IN STD_LOGIC;
+    rho : OUT STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+    theta : OUT STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+    valid : OUT STD_LOGIC
+  );
 
 END ENTITY;
 
 ARCHITECTURE behavioral OF CORDIC IS
 
-    -- CONSTANT k : SIGNED(N - 1 DOWNTO 0) := to_signed(1304065748, N); -- 1/(Gain factor) multiplied by 2^N-1
-    CONSTANT k : UNSIGNED(M - 1 DOWNTO 0) := to_unsigned(INTEGER(0.6072528458 * (2 ** (M - 1))), M); -- todo se in futuro vivado si lamenta usare M-2 
-    CONSTANT HALF_PI : SIGNED(M - 1 DOWNTO 0) := to_signed(INTEGER(1.570796327 * (2 ** (M - 3))), M); -- todo documentare meglio il N-3  
+  -- CONSTANT k : SIGNED(N - 1 DOWNTO 0) := to_signed(1304065748, N); -- 1/(Gain factor) multiplied by 2^N-1
+  CONSTANT k : UNSIGNED(M - 1 DOWNTO 0) := to_unsigned(INTEGER(0.6072528458 * (2 ** (M - 1))), M); -- todo se in futuro vivado si lamenta usare M-2 
+  CONSTANT HALF_PI : SIGNED(M - 1 DOWNTO 0) := to_signed(INTEGER(1.570796327 * (2 ** (M - 3))), M); -- todo documentare meglio il N-3  
 
-    -- internal registers
-    SIGNAL x_t : SIGNED(M - 1 DOWNTO 0);
-    SIGNAL y_t : SIGNED(M - 1 DOWNTO 0);
-    SIGNAL z_t : SIGNED(M - 1 DOWNTO 0);
+  -- internal registers
+  SIGNAL x_t : SIGNED(M - 1 DOWNTO 0);
+  SIGNAL y_t : SIGNED(M - 1 DOWNTO 0);
+  SIGNAL z_t : SIGNED(M - 1 DOWNTO 0);
 
-    -- output registers
-    SIGNAL x_out : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
-    SIGNAL z_out : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+  -- output registers
+  SIGNAL x_out : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+  SIGNAL z_out : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
 
-    -- atan table address and output
-    SIGNAL address : STD_LOGIC_VECTOR(ITER_BITS - 1 DOWNTO 0);
-    SIGNAL atan_out : STD_LOGIC_VECTOR(M - 1 DOWNTO 0);
+  -- atan table address and output
+  SIGNAL address : STD_LOGIC_VECTOR(ITER_BITS - 1 DOWNTO 0);
+  SIGNAL atan_out : STD_LOGIC_VECTOR(M - 1 DOWNTO 0);
 
-    SIGNAL sign : STD_LOGIC;
+  SIGNAL sign : STD_LOGIC;
 
-    -- atan table
-    COMPONENT ATAN_LUT IS
-        PORT (
-            address : IN STD_LOGIC_VECTOR(ITER_BITS - 1 DOWNTO 0);
-            lut_out : OUT STD_LOGIC_VECTOR(M - 1 DOWNTO 0)
-        );
-    END COMPONENT;
+  -- atan table
+  COMPONENT ATAN_LUT IS
+    PORT (
+      address : IN STD_LOGIC_VECTOR(ITER_BITS - 1 DOWNTO 0);
+      lut_out : OUT STD_LOGIC_VECTOR(M - 1 DOWNTO 0)
+    );
+  END COMPONENT;
 
-    -- state type and registers
-    TYPE state_t IS (WAITING, FIX, COMPUTING, FINISHED);
-    SIGNAL current_state : state_t;
+  -- state type and registers
+  TYPE state_t IS (WAITING, FIX, COMPUTING, FINISHED);
+  SIGNAL current_state : state_t;
 
-    -- counter for iterations
-    SIGNAL counter : UNSIGNED(ITER_BITS - 1 DOWNTO 0);
+  -- counter for iterations
+  SIGNAL counter : UNSIGNED(ITER_BITS - 1 DOWNTO 0);
 
 BEGIN
-    ----------------------------------------------
-    -- ISTANTIATE ALL COMPONENTS
-    ----------------------------------------------
+  ----------------------------------------------
+  -- ISTANTIATE ALL COMPONENTS
+  ----------------------------------------------
 
-    -- output assignment
-    rho <= x_out;
-    theta <= z_out;
+  -- output assignment
+  rho <= x_out;
+  theta <= z_out;
 
-    -- todo trovare nome migliore tipo d
-    -- sign bit
-    sign <= y_t(M - 1);
-    -- sign <= '0' WHEN y_t > 0 ELSE '1';
+  -- todo trovare nome migliore tipo d
+  -- sign bit
+  sign <= y_t(M - 1);
+  -- sign <= '0' WHEN y_t > 0 ELSE '1';
 
-    -- atan table
-    atan_lut_inst : ATAN_LUT
-    PORT MAP(
-        address => address,
-        lut_out => atan_out
-    );
+  -- atan table
+  atan_lut_inst : ATAN_LUT
+  PORT MAP(
+    address => address,
+    lut_out => atan_out
+  );
 
-    -- atan table address
-    address <= STD_LOGIC_VECTOR(counter);
+  -- atan table address
+  address <= STD_LOGIC_VECTOR(counter);
 
-    -- todo decidere se tenere o togliere gli assegnamenti stupidi
-    -- todo forse z dovrebbe avere solo 2 bit interi e il resto frazionari
-    -- aggiustare meglio spiegazione e codice per i 29 bit di atan
+  -- todo decidere se tenere o togliere gli assegnamenti stupidi
+  -- aggiustare meglio spiegazione e codice per i 29 bit di atan
 
-    ----------------------------------------------
-    -- CONTROL PART
-    ----------------------------------------------
+  ----------------------------------------------
+  -- CONTROL PART
+  ----------------------------------------------
 
-    control : PROCESS (clk, rst)
-    BEGIN
-        IF (rising_edge(clk)) THEN
-            IF rst = '1' THEN
-                current_state <= WAITING;
+  control : PROCESS (clk, rst)
+  BEGIN
+    IF (rising_edge(clk)) THEN
+      IF rst = '1' THEN
+        current_state <= WAITING;
+      ELSE
+
+        CASE current_state IS
+          WHEN WAITING =>
+            IF start = '1' THEN
+              current_state <= FIX;
             ELSE
-
-                CASE current_state IS
-                    WHEN WAITING =>
-                        IF start = '1' THEN
-                            current_state <= FIX;
-                        ELSE
-                            current_state <= WAITING;
-                        END IF;
-
-                    WHEN FIX =>
-                        current_state <= COMPUTING;
-
-                    WHEN COMPUTING =>
-                        IF counter = ITERATIONS - 1 THEN
-                            current_state <= FINISHED;
-                        ELSE
-                            current_state <= COMPUTING;
-                        END IF;
-
-                    WHEN FINISHED =>
-                        current_state <= WAITING;
-
-                    WHEN OTHERS =>
-                        current_state <= WAITING;
-
-                END CASE;
+              current_state <= WAITING;
             END IF;
-        END IF;
-    END PROCESS;
 
-    ----------------------------------------------
-    -- OPEATIONAL PART
-    ----------------------------------------------
+          WHEN FIX =>
+            current_state <= COMPUTING;
 
-    OPEATIONAL : PROCESS (clk, rst)
-    BEGIN
-        IF (rising_edge(clk)) THEN
-            IF rst = '1' THEN
-                valid <= '0';
-                x_out <= (OTHERS => '0');
-                z_out <= (OTHERS => '0');
-                counter <= (OTHERS => '0');
-                x_t <= (OTHERS => '0');
-                y_t <= (OTHERS => '0');
-                z_t <= (OTHERS => '0');
+          WHEN COMPUTING =>
+            IF counter = ITERATIONS - 1 THEN
+              current_state <= FINISHED;
             ELSE
-
-                -- Default assignment
-                -- todo vedere se tenere o togliere
-                -- x_t <= (OTHERS => '-');
-                -- y_t <= (OTHERS => '-');
-                -- z_t <= (OTHERS => '-');
-                -- x_out <= (OTHERS => '-');
-                -- z_out <= (OTHERS => '-');
-                counter <= (OTHERS => '0');
-
-                CASE current_state IS
-                    WHEN WAITING =>
-                        x_t <= shift_left(resize(signed(x), M), (M - N - 2));   -- Internal representation has 14 bits of fraction
-                        y_t <= shift_left(resize(signed(y), M), (M - N - 2));
-                        z_t <= to_signed(0, M);
-                        valid <= '1';
-                        x_out <= x_out;
-                        z_out <= z_out;
-
-                    WHEN FIX =>
-                        IF sign = '1' THEN
-                            x_t <= - y_t;
-                            y_t <= x_t;
-                            z_t <= z_t - HALF_PI;
-                        ELSE
-                            x_t <= y_t;
-                            y_t <= - x_t;
-                            z_t <= z_t + HALF_PI;
-                        END IF;
-
-                        valid <= '0';
-                        counter <= (OTHERS => '0');
-
-                    WHEN COMPUTING =>
-                        IF sign = '1' THEN
-                            -- x_t <= x_t - y_t/(2 ** to_integer(counter));
-                            x_t <= x_t - shift_right(y_t, to_integer(counter));
-                            -- y_t <= y_t + x_t/(2 ** to_integer(counter));
-                            y_t <= y_t + shift_right(x_t, to_integer(counter));
-                            z_t <= z_t - signed(atan_out);
-                        ELSE
-                            -- x_t <= x_t + y_t/(2 ** to_integer(counter));
-                            x_t <= x_t + shift_right(y_t, to_integer(counter));
-                            -- y_t <= y_t - x_t/(2 ** to_integer(counter));
-                            y_t <= y_t - shift_right(x_t, to_integer(counter));
-                            z_t <= z_t + signed(atan_out);
-                        END IF;
-
-                        IF counter < ITERATIONS - 1 THEN
-                            counter <= counter + 1;
-                        ELSE
-                            counter <= (OTHERS => '0');
-                        END IF;
-
-                        valid <= '0';
-
-                    WHEN FINISHED =>
-                        x_out <= STD_LOGIC_VECTOR(resize(unsigned(x_t) * k / (2 ** (M - 1)), M)(M - 1 - 2 DOWNTO M - N - 2));
-                        z_out <= STD_LOGIC_VECTOR(z_t(M - 1 DOWNTO M - N));
-
-                        valid <= '1';
-
-                END CASE;
+              current_state <= COMPUTING;
             END IF;
-        END IF;
-    END PROCESS;
+
+          WHEN FINISHED =>
+            current_state <= WAITING;
+
+          WHEN OTHERS =>
+            current_state <= WAITING;
+
+        END CASE;
+      END IF;
+    END IF;
+  END PROCESS;
+
+  ----------------------------------------------
+  -- OPEATIONAL PART
+  ----------------------------------------------
+
+  OPEATIONAL : PROCESS (clk, rst)
+  BEGIN
+    IF (rising_edge(clk)) THEN
+      IF rst = '1' THEN
+        valid <= '0';
+        x_out <= (OTHERS => '0');
+        z_out <= (OTHERS => '0');
+        counter <= (OTHERS => '0');
+        x_t <= (OTHERS => '0');
+        y_t <= (OTHERS => '0');
+        z_t <= (OTHERS => '0');
+      ELSE
+
+        -- Default assignment
+        -- todo vedere se tenere o togliere
+        -- x_t <= (OTHERS => '-');
+        -- y_t <= (OTHERS => '-');
+        -- z_t <= (OTHERS => '-');
+        -- x_out <= (OTHERS => '-');
+        -- z_out <= (OTHERS => '-');
+        counter <= (OTHERS => '0');
+
+        CASE current_state IS
+          WHEN WAITING =>
+            x_t <= shift_left(resize(signed(x), M), (M - N - 2)); -- Internal representation has 14 bits of fraction
+            y_t <= shift_left(resize(signed(y), M), (M - N - 2));
+            z_t <= to_signed(0, M);
+            valid <= '1';
+            x_out <= x_out;
+            z_out <= z_out;
+
+          WHEN FIX =>
+            IF sign = '1' THEN
+              x_t <= - y_t;
+              y_t <= x_t;
+              z_t <= z_t - HALF_PI;
+            ELSE
+              x_t <= y_t;
+              y_t <= - x_t;
+              z_t <= z_t + HALF_PI;
+            END IF;
+
+            valid <= '0';
+            counter <= (OTHERS => '0');
+
+          WHEN COMPUTING =>
+            IF sign = '1' THEN
+              -- x_t <= x_t - y_t/(2 ** to_integer(counter));
+              x_t <= x_t - shift_right(y_t, to_integer(counter));
+              -- y_t <= y_t + x_t/(2 ** to_integer(counter));
+              y_t <= y_t + shift_right(x_t, to_integer(counter));
+              z_t <= z_t - signed(atan_out);
+            ELSE
+              -- x_t <= x_t + y_t/(2 ** to_integer(counter));
+              x_t <= x_t + shift_right(y_t, to_integer(counter));
+              -- y_t <= y_t - x_t/(2 ** to_integer(counter));
+              y_t <= y_t - shift_right(x_t, to_integer(counter));
+              z_t <= z_t + signed(atan_out);
+            END IF;
+
+            IF counter < ITERATIONS - 1 THEN
+              counter <= counter + 1;
+            ELSE
+              counter <= (OTHERS => '0');
+            END IF;
+
+            valid <= '0';
+
+          WHEN FINISHED =>
+            x_out <= STD_LOGIC_VECTOR(resize(unsigned(x_t) * k / (2 ** (M - 1)), M)(M - 1 - 2 DOWNTO M - N - 2));
+            z_out <= STD_LOGIC_VECTOR(z_t(M - 1 DOWNTO M - N));
+
+            valid <= '1';
+
+        END CASE;
+      END IF;
+    END IF;
+  END PROCESS;
 END ARCHITECTURE;
--- vhdl/tb/GRAPH_tb.vhd
+++ vhdl/tb/GRAPH_tb.vhd
@@ -1,173 +1,173 @@
-library ieee;
-use ieee.std_logic_1164.all;
-use ieee.numeric_std.all;
-use ieee.math_real.all;
-use std.textio.all;  -- Per scrivere su file di testo
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE ieee.math_real.ALL;
+USE std.textio.ALL; -- Per scrivere su file di testo
 
-entity GRAPH_tb is
-    generic (
-        N : positive := 16;      -- larghezza del bus di ingresso e uscita
-        floating : integer := 8  -- numero di bit frazionari (Q8.8)
+ENTITY GRAPH_tb IS
+  GENERIC (
+    N : POSITIVE := 16; -- larghezza del bus di ingresso e uscita
+    floating : INTEGER := 8 -- numero di bit frazionari (Q8.8)
+  );
+END ENTITY;
+
+ARCHITECTURE Behavioral OF GRAPH_tb IS
+
+  ----------------------------------------------------------------------------
+  -- Dichiarazione componente CORDIC
+  ----------------------------------------------------------------------------
+  COMPONENT CORDIC
+    PORT (
+      clk : IN STD_LOGIC;
+      rst : IN STD_LOGIC;
+      x : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+      y : IN STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+      start : IN STD_LOGIC;
+      rho : OUT STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+      theta : OUT STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+      valid : OUT STD_LOGIC
     );
-end entity;
+  END COMPONENT;
 
-architecture Behavioral of GRAPH_tb is
+  ----------------------------------------------------------------------------
+  -- Segnali locali
+  ----------------------------------------------------------------------------
+  SIGNAL clk : STD_LOGIC := '0';
+  SIGNAL rst : STD_LOGIC := '0';
+  SIGNAL x_in : STD_LOGIC_VECTOR(N - 1 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL y_in : STD_LOGIC_VECTOR(N - 1 DOWNTO 0) := (OTHERS => '0');
+  SIGNAL start_in : STD_LOGIC := '0';
+  SIGNAL rho_out : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+  SIGNAL theta_out : STD_LOGIC_VECTOR(N - 1 DOWNTO 0);
+  SIGNAL valid_out : STD_LOGIC := '0';
+
+  SIGNAL run_simulation : STD_LOGIC := '1';
+
+  ----------------------------------------------------------------------------
+  -- Parametri per la simulazione
+  ----------------------------------------------------------------------------
+  CONSTANT T_clk : TIME := 10 ns; -- periodo di clock = 10 ns
+  CONSTANT SAMPLES : INTEGER := 512; -- numero di campioni in ogni dimensione
+
+  ----------------------------------------------------------------------------
+  -- File di output
+  ----------------------------------------------------------------------------
+  FILE results_file : text OPEN write_mode IS "cordic_results_512x512.txt";
+
+  ----------------------------------------------------------------------------
+  -- Funzione di conversione da std_logic_vector (signed) a real,
+  -- tenendo conto del numero di bit frazionario.
+  ----------------------------------------------------------------------------
+  FUNCTION u_to_real(val : unsigned(N - 1 DOWNTO 0); fraction_bits : INTEGER) RETURN real IS
+  BEGIN
+    RETURN real(to_integer(val)) / 2.0 ** fraction_bits;
+  END FUNCTION;
+
+  FUNCTION s_to_real(val : signed(N - 1 DOWNTO 0); fraction_bits : INTEGER) RETURN real IS
+  BEGIN
+    RETURN real(to_integer(val)) / 2.0 ** fraction_bits;
+  END FUNCTION;
+
+BEGIN
+
+  ----------------------------------------------------------------------------
+  -- Istanza del CORDIC
+  ----------------------------------------------------------------------------
+  UUT : CORDIC
+  PORT MAP(
+    clk => clk,
+    rst => rst,
+    x => x_in,
+    y => y_in,
+    start => start_in,
+    rho => rho_out,
+    theta => theta_out,
+    valid => valid_out
+  );
+
+  ----------------------------------------------------------------------------
+  -- Generazione del clock
+  ----------------------------------------------------------------------------
+  clk_process : PROCESS
+  BEGIN
+    WHILE run_simulation = '1' LOOP
+      clk <= '1';
+      WAIT FOR T_clk/2;
+      clk <= '0';
+      WAIT FOR T_clk/2;
+    END LOOP;
+    WAIT; -- fermo il processo quando run_simulation = '0'
+  END PROCESS clk_process;
+
+  ----------------------------------------------------------------------------
+  -- Processo di test
+  ----------------------------------------------------------------------------
+  test_process : PROCESS
+    VARIABLE L : line;
+    VARIABLE i, j : INTEGER;
+    VARIABLE real_x, real_y : real;
+    VARIABLE mod_val, phase_val : real;
+  BEGIN
+    ----------------------------------------------------------------------------
+    -- Reset iniziale
+    ----------------------------------------------------------------------------
+    rst <= '1';
+    start_in <= '0';
+    WAIT FOR 5 * T_clk;
+    rst <= '0';
+    WAIT FOR 5 * T_clk;
 
     ----------------------------------------------------------------------------
-    -- Dichiarazione componente CORDIC
+    -- Loop su (i, j) per testare tutti i valori
     ----------------------------------------------------------------------------
-    component CORDIC
-        port (
-            clk   : in  std_logic;
-            rst   : in  std_logic;
-            x     : in  std_logic_vector(N - 1 downto 0);
-            y     : in  std_logic_vector(N - 1 downto 0);
-            start : in  std_logic;
-            rho   : out std_logic_vector(N - 1 downto 0);
-            theta : out std_logic_vector(N - 1 downto 0);
-            valid : out std_logic
-        );
-    end component;
+    FOR i IN 0 TO SAMPLES - 1 LOOP
+      FOR j IN 0 TO SAMPLES - 1 LOOP
 
-    ----------------------------------------------------------------------------
-    -- Segnali locali
-    ----------------------------------------------------------------------------
-    signal clk          : std_logic := '0';
-    signal rst          : std_logic := '0';
-    signal x_in         : std_logic_vector(N - 1 downto 0) := (others => '0');
-    signal y_in         : std_logic_vector(N - 1 downto 0) := (others => '0');
-    signal start_in     : std_logic := '0';
-    signal rho_out      : std_logic_vector(N - 1 downto 0);
-    signal theta_out    : std_logic_vector(N - 1 downto 0);
-    signal valid_out    : std_logic := '0';
+        -- Calcolo dei valori reali corrispondenti
+        real_x := - 128.0 + eral(i) * 256.0 / real(SAMPLES);
+        real_y := - 128.0 + real(j) * 256.0 / real(SAMPLES);
 
-    signal run_simulation : std_logic := '1';
+        -- Conversione in Q8.8 (signed)
+        x_in <= STD_LOGIC_VECTOR(to_signed(INTEGER(floor(real_x * 2.0 ** floating)), N));
+        y_in <= STD_LOGIC_VECTOR(to_signed(INTEGER(floor(real_y * 2.0 ** floating)), N));
 
-    ----------------------------------------------------------------------------
-    -- Parametri per la simulazione
-    ----------------------------------------------------------------------------
-    constant T_clk      : time := 10 ns;  -- periodo di clock = 10 ns
-    constant SAMPLES    : integer := 512; -- numero di campioni in ogni dimensione
-
-    ----------------------------------------------------------------------------
-    -- File di output
-    ----------------------------------------------------------------------------
-    file results_file : text open write_mode is "cordic_results_512x512.txt";
-
-    ----------------------------------------------------------------------------
-    -- Funzione di conversione da std_logic_vector (signed) a real,
-    -- tenendo conto del numero di bit frazionario.
-    ----------------------------------------------------------------------------
-    function u_to_real(val : unsigned(N-1 downto 0); fraction_bits : integer) return real is
-    begin
-        return real(to_integer(val)) / 2.0**fraction_bits;
-    end function;
-
-    function s_to_real(val : signed(N-1 downto 0); fraction_bits : integer) return real is
-    begin
-        return real(to_integer(val)) / 2.0**fraction_bits;
-    end function;
-
-begin
-
-    ----------------------------------------------------------------------------
-    -- Istanza del CORDIC
-    ----------------------------------------------------------------------------
-    UUT : CORDIC
-        port map(
-            clk   => clk,
-            rst   => rst,
-            x     => x_in,
-            y     => y_in,
-            start => start_in,
-            rho   => rho_out,
-            theta => theta_out,
-            valid => valid_out
-        );
-
-    ----------------------------------------------------------------------------
-    -- Generazione del clock
-    ----------------------------------------------------------------------------
-    clk_process : process
-    begin
-        while run_simulation = '1' loop
-            clk <= '1';
-            wait for T_clk/2;
-            clk <= '0';
-            wait for T_clk/2;
-        end loop;
-        wait;  -- fermo il processo quando run_simulation = '0'
-    end process clk_process;
-
-    ----------------------------------------------------------------------------
-    -- Processo di test
-    ----------------------------------------------------------------------------
-    test_process : process
-    variable L : line;
-    variable i, j : integer;
-    variable real_x, real_y : real;
-    variable mod_val, phase_val : real;
-    begin
-        ----------------------------------------------------------------------------
-        -- Reset iniziale
-        ----------------------------------------------------------------------------
-        rst <= '1';
+        -- Avvio CORDIC
+        start_in <= '1';
+        WAIT FOR T_clk;
         start_in <= '0';
-        wait for 5*T_clk;
-        rst <= '0';
-        wait for 5*T_clk;
+
+        -- Attendiamo che valid_out passi a '1'
+        WAIT UNTIL valid_out = '1';
+        WAIT FOR 10 ns;
+
+        -- Acquisisco il risultato
+        mod_val := u_to_real(unsigned(rho_out), 8); -- Q8.8
+        phase_val := s_to_real(signed(theta_out), 13); -- Q3.13
 
         ----------------------------------------------------------------------------
-        -- Loop su (i, j) per testare tutti i valori
+        -- Stampo sul file di testo: x, y, rho, theta
         ----------------------------------------------------------------------------
-        for i in 0 to SAMPLES-1 loop
-            for j in 0 to SAMPLES-1 loop
+        -- Scrivi i valori separati da virgola
+        write(L, real_x, RIGHT, 0, 6);
+        write(L, STRING'(","));
+        write(L, real_y, RIGHT, 0, 6);
+        write(L, STRING'(","));
+        write(L, mod_val, RIGHT, 0, 6);
+        write(L, STRING'(","));
+        write(L, phase_val, RIGHT, 0, 6);
+        writeline(results_file, L);
 
-                -- Calcolo dei valori reali corrispondenti
-                real_x := -128.0 + eral(i) * 256.0 / real(SAMPLES);
-                real_y := -128.0 + real(j) * 256.0 / real(SAMPLES);
+        -- Piccola attesa fra un test e il successivo
+        WAIT FOR 1 * T_clk;
 
-                -- Conversione in Q8.8 (signed)
-                x_in <= std_logic_vector(to_signed(integer(floor(real_x * 2.0**floating)), N));
-                y_in <= std_logic_vector(to_signed(integer(floor(real_y * 2.0**floating)), N));
+      END LOOP;
+    END LOOP;
 
-                -- Avvio CORDIC
-                start_in <= '1';
-                wait for T_clk;
-                start_in <= '0';
+    ----------------------------------------------------------------------------
+    -- Fine simulazione
+    ----------------------------------------------------------------------------
+    run_simulation <= '0';
+    WAIT;
+  END PROCESS test_process;
 
-                -- Attendiamo che valid_out passi a '1'
-                wait until valid_out = '1';
-                wait for 10 ns;
-
-                -- Acquisisco il risultato
-                mod_val   := u_to_real(unsigned(rho_out), 8);     -- Q8.8
-                phase_val := s_to_real(signed(theta_out), 13);  -- Q3.13
-
-                ----------------------------------------------------------------------------
-                -- Stampo sul file di testo: x, y, rho, theta
-                ----------------------------------------------------------------------------
-                -- Scrivi i valori separati da virgola
-                write(L, real_x, RIGHT, 0, 6);
-                write(L, string'(","));
-                write(L, real_y, RIGHT, 0, 6);
-                write(L, string'(","));
-                write(L, mod_val, RIGHT, 0, 6);
-                write(L, string'(","));
-                write(L, phase_val, RIGHT, 0, 6);
-                writeline(results_file, L);
-
-                -- Piccola attesa fra un test e il successivo
-                wait for 1*T_clk;
-
-            end loop;
-        end loop;
-
-        ----------------------------------------------------------------------------
-        -- Fine simulazione
-        ----------------------------------------------------------------------------
-        run_simulation <= '0';
-        wait;
-    end process test_process;
-
-end Behavioral;
+END Behavioral;
